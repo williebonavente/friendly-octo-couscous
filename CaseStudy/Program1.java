3e0c1ee2814b6d12ec2d39d51192e73983449eaa
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2024-03-16 23:51:14 +0800

    Finishing Program 1

--- CaseStudy/Program1.java
+++ CaseStudy/Program1.java
@@ -27,31 +27,51 @@ public class Program1 {
         int selectProblem = in.nextInt();
 
         // implement the do while here
+        do {
+            switch (selectProblem) {
+                case 1:
+                    double[] rate = get_rate_drop_factor();
+                    double rate_ml_hr = rate[0];
+                    double dropFactor = rate[1];
+                    System.out.println("The drop rate per minute is " + fig_drops_min(rate_ml_hr, dropFactor) + ".\n");
+                    break;
+                case 2:
+                    double[] hour = get_fig_ml();
+                    double get_hour = hour[0];
+                    System.out.println("The rate in milliliters per hour is " + fig_ml_hr(get_hour) + ".\n");
 
-        switch (selectProblem) {
-            case 1:
-                double[] results = get_rate_drop_factor();
-                double rate_ml_hr = results[0];
-                double dropFactor = results[1];
-                System.out.println( "The drop rate per minute is "+ fig_drops_min(rate_ml_hr, dropFactor) + ".");
-                break;
-            case 2:
-                break;
-            case 3:
-                get_kg_rate_conc();
-                break;
-            case 4:
-                get_units_conc();
-                break; // Add a break statement here
-            case 5:
-            default:
-                break;
-        }
+                    break;
+                case 3:
+                    double[] weight = get_kg_rate_conc();
+                    double rate_ml_kg_hr = weight[0];
+                    double weight_kg = weight[1];
+                    double concentration = weight[2];
+                    System.out.println(
+                            "The drop rate per minute is " + by_weight(rate_ml_kg_hr, weight_kg, concentration) + ".\n");
+                    break;
+                case 4:
+                    double[] unit = get_units_conc();
+                    double units_hr = unit[0];
+                    double units_ml = unit[1];
+                    System.out.println("The rate in milliliters is per hour  is " + by_units(units_ml, units_hr) + ".\n");
+                    break; // Add a break statement here
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println();
+                    System.out.printf("%50s","Please input a valid number (1-5)");
+                    System.out.println();
+                    break;
+            }
+            
+            System.out.println();
+            get_problem();
+            System.out.print("Problem => ");
+            selectProblem = in.nextInt();
 
-        // insert do while here
-        
+        } while (selectProblem != 5);
+
         closeScanner();
-
     }
 
     // prompting menu
@@ -63,10 +83,8 @@ public class Program1 {
         System.out.printf("%3s %35s %15s", "(3)", "mg/kg/hr & concentration in mg/ml", "ml / hr\n");
         System.out.printf("%3s %38s %12s", "(4)", "units/hr & concentration in units/ml", "ml / hr\n");
         System.out.printf("%3s %7s", "(5)", "QUIT\n");
-        System.out.println();
     }
 
-    // for problem 1
     // for problem 1
     static double[] get_rate_drop_factor() {
         System.out.print("Enter rate in ml/hr=> ");
@@ -77,18 +95,40 @@ public class Program1 {
         return new double[] { rate_ml_hr, dropFactor };
     }
 
+    // for problem 2
+    static double[] get_fig_ml() {
+        System.out.print("Enter number of hours=> ");
+        double hour = in.nextDouble();
+
+        return new double[] { hour };
+    }
+
     // for problem 3
-    static double get_kg_rate_conc() {
-        return 0;
+    static double[] get_kg_rate_conc() {
+        System.out.print("Enter rate in mg/kg/hr=> ");
+        double rate_ml_kg_hr = in.nextDouble();
+        System.out.print("Enter patient weight in kg=> ");
+        double weight = in.nextDouble();
+        System.out.print("Enter concentration in mg/ml=> ");
+        double concentration = in.nextDouble();
+
+        return new double[] { rate_ml_kg_hr, weight, concentration };
     }
 
     // for problem 4
-    static double get_units_conc() {
-        return 0;
+    static double[] get_units_conc() {
+
+        System.out.print("Enter rate in units/hr=> ");
+        double rate_unit_hr = in.nextDouble();
+        System.out.print("Enter concentration in units/ml=> ");
+        double units_ml = in.nextDouble();
+
+        return new double[] { rate_unit_hr, units_ml };
+
     }
 
     /**
-     * Conversion Functions
+     * Calculation Functions
      * 
      * 
      * Takes rate and drop factor as input parameters and returns drops/min (rounded
@@ -103,8 +143,11 @@ public class Program1 {
      * Takes as an input parameter the number of hours over which one liter is to be
      * delivered and returns ml/hr (rounded) as function value.
      */
-    static double fig_ml_hr() {
-        return 0;
+    static int fig_ml_hr(double hours) {
+
+        double rate_ml_ph = 1000 / hours;
+        return (int) rate_ml_ph;
+
     }
 
     /*
@@ -112,8 +155,9 @@ public class Program1 {
      * concentration
      * of drug in mg/ml and returns ml/hr (rounded) as function value.
      */
-    static double by_weight() {
-        return 0;
+    static double by_weight(double rate_ml_kg_hr, double weight, double concentration) {
+        double rate_ml_hr = rate_ml_kg_hr * weight * concentration;
+        return (int) rate_ml_hr;
     }
 
     /*
@@ -121,8 +165,9 @@ public class Program1 {
      * returns
      * ml/hr (rounded) as function value.
      */
-    static double by_units() {
-        return 0;
+    static int by_units(double units_ml, double units_hr) {
+        double ml_hr = units_hr / units_ml;
+        return (int) ml_hr;
     }
 
     public static void closeScanner() {
